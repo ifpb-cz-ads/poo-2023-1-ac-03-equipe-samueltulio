23594f610b2b2bc074fc464cb11e414c6fbf360b
flaviotulioalmeida <138700144+flaviotulioalmeida@users.noreply.github.com>
2023-08-26 14:11:48 -0300

    Adicionando atividade do livro CBB pratica 2

---
+++ CBBAtividadePratica2.java
@@ -0,0 +1,11 @@
+
+
+public class CBBAtividadePratica2{
+    public static void main(String[] args){
+        int nota1 = 5;
+        int nota2 = 7;
+        int nota3 = 8;
+        int media = (nota1+nota2+nota3)/3;
+        System.out.print("A media é:" + media);
+    }
+}
