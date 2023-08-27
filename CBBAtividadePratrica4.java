01398c5875b682d40238cb170e51083838095907
flaviotulioalmeida <138700144+flaviotulioalmeida@users.noreply.github.com>
2023-08-27 09:19:22 -0300

    Adicionando Atividade do livro CBB pratica 4

---
+++ CBBAtividadePratrica4.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class CBBAtividadePratrica4{
+     public static final float CEM = 100f;
+     public static void main(String[] args){
+         Scanner scanner = new Scanner(System.in);
+         System.out.println("Calculadora de Orçamento de Piscina");
+         System.out.print("Informe o comprimento da piscina (metros): ");
+         double comprimento = scanner.nextDouble();
+         System.out.print("Informe a largura da piscina (metros): ");
+         double largura = scanner.nextDouble();
+         System.out.print("Informe a profundidade da piscina (metros): ");
+         double profundidade = scanner.nextDouble();
+         double cubagem = comprimento * largura * profundidade;
+         double valorTotal = cubagem * CEM;
+         System.out.printf("O valor total para a construção da piscina é de R$%.2f",valorTotal);
+         
+         
+     }
+ 
+}
