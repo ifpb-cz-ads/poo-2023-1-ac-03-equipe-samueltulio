dc8f7cfb0738c91ca246702187ecb6b41f1d4a50
flaviotulioalmeida <138700144+flaviotulioalmeida@users.noreply.github.com>
2023-08-30 17:16:39 -0300

    Update CBBAtividadePratica3.java

--- CBBAtividadePratica3.java
+++ CBBAtividadePratica3.java
@@ -2,12 +2,12 @@ import java.util.Scanner;
 public class CBBAtividadePratica3{
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Informe a base de um triangulo:");
+        System.out.print("Informe a base de um retângulo:");
         double base = scanner.nextDouble();
-        System.out.print("Informe a altura de um triangulo:");
+        System.out.print("Informe a altura de um retângulo:");
         double altura = scanner.nextDouble();
         double area = (base*altura)/2;
-        System.out.println("A Area do triangulo é:" + area);
+        System.out.println("A Area do retângulo é:" + area);
     }
    
 }
